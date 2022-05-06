8aa9bbc3a8abc0b1b7ac00ee1aac6362e5e32f11
Edilon Abranches <edilonab@hotmail.com>
2022-05-06 15:58:39 +0000

    componentes

--- Classe/OrientacaoObjeto.php
+++ Classe/OrientacaoObjeto.php
@@ -1,19 +1,25 @@
 <?php
-Class Login {
+class Login {
     public $name; 
-    public $idade;
-    public $email; 
+    public $email;
+    public $senha; 
 
-    public function Logar  (){
-        if (this=>name,idade , email,);
+    public function Logar() {
+        if($this->email == "teste@testa.com" and $this-> senha == "123456",):
+            echo "logado com sucesso!";
+        else:
+            echo "Dados inválidos";
+        endif;
+    };
+
     
     }
 }
 
 
-/ *Orientação a objeto
+    /*Orientação a objeto
 
-Crie uma classe contendo 3 propriedades com seus respectivos 
-gets e sets e um método que multiplique as 3 retornando o produto.
- Deixe um exemplo de utilização da sua classe no final do código.*/
- ?>
+    Crie uma classe contendo 3 propriedades com seus respectivos 
+    gets e sets e um método que multiplique as 3 retornando o produto.
+     Deixe um exemplo de utilização da sua classe no final do código.*/
+?>
