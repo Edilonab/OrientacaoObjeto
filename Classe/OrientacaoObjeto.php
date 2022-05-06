06656dc0607e6c8acb0771caa328a31dee30b5c2
Edilon Abranches <edilonab@hotmail.com>
2022-05-06 16:36:49 +0000

    get set

--- Classe/OrientacaoObjeto.php
+++ Classe/OrientacaoObjeto.php
@@ -1,21 +1,42 @@
 <?php
-class Login {
-    public $name; 
-    public $email;
-    public $senha; 
+    class Login {
+        private $name; 
+        private $email;
+        private $senha; 
 
-    public function Logar() {
-        if($this->email == "teste@testa.com" and $this-> senha == "123456",):
-            echo "logado com sucesso!";
-        else:
-            echo "Dados inválidos";
-        endif;
-    };
+        public function getEmail() {
+            return $this->email;
+        }
 
-    
+        public function setEmail() {
+            $email = filter_var($e, FILTER_SANITIZE_EMAIL)
+            $this->email = $email;
+        }
+
+        public function getsenha() {
+            return $this->senha;
+        }
+
+        public function setsenha($s) {
+            $this->senha;
+        }
+
+
+        public function Logar() {
+            if($this->email == "teste@testa.com" and $this-> senha == "123456",):
+                echo "logado com sucesso!";
+            else:
+                echo "Dados inválidos";
+            endif;
+        }
     }
-}
 
+    $logar = new Login();
+    $logar->setEmail("teste()/@teste.com");
+    $logar->setSenha("123456");
+    $logar->Logar();
+    echo "<br>";
+    echo $logar->getEmail();
 
     /*Orientação a objeto
 
